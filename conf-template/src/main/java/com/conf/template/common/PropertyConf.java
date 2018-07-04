6b72f7e895ce3c7a7962dfa47c0f3fde427c876b
HOPERUN-DOMAIN\li_mingxing <li_mingxing@hoperun.com>
2018-07-04 17:09:43 +0800

    新增类

---
+++ conf-template/src/main/java/com/conf/template/common/PropertyConf.java
@@ -0,0 +1,25 @@
+package com.conf.template.common;
+
+/**
+ * 配置类
+ * 
+ * @author  sunliang
+ * @version  [版本号, 2018年7月4日]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+public class PropertyConf
+{
+    private String scanPackageName;
+
+    public String getScanPackageName()
+    {
+        return scanPackageName == null ? Constants.SCAN_PACKAGE_NAME : scanPackageName;
+    }
+
+    public void setScanPackageName(String scanPackageName)
+    {
+        this.scanPackageName = scanPackageName;
+    }
+
+}

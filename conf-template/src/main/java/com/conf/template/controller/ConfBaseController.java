be049540d29a69766b3435f014440e069896a12a
HOPERUN-DOMAIN\li_mingxing <li_mingxing@hoperun.com>
2018-07-12 16:39:41 +0800

    代码提交

---
+++ conf-template/src/main/java/com/conf/template/controller/ConfBaseController.java
@@ -0,0 +1,9 @@
+package com.conf.template.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfBaseController
+{
+    
+}
---
+++ conf-template/src/main/java/com/conf/template/service/ConfBaseService.java
@@ -0,0 +1,9 @@
+package com.conf.template.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfBaseService
+{
+    
+}
